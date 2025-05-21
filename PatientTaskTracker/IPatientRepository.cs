abb8ba7e72b3f795a82e595c58ebc2e13e484b05
Honore <hsolo11@wgu.edu>
2025-05-20 21:05:07 -0700

    Added IPatientRepository interface with methods

---
+++ PatientTaskTracker/IPatientRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace PatientTaskTracker
+{
+    interface IPatientRepository
+    {
+        public bool AddPatient(Patient patient);
+        public IEnumerable<Patient> GetAllPatients();
+        public bool EditPatient(int patientId, string newFirstName, string newLastName);
+        public bool RemovePatient(int patientId);
+        public bool PatientExists(int patientId);
+        public Patient FindPatientById(int patientId);
+
+    }
+}
